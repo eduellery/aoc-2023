826f624fbdc576d275c0e4613fd164e424d54793
Eduardo Ellery <eduellery@gmail.com>
2023-12-02 07:42:19 +0100

    2023 - Day 01 - Part 2

--- src/Day01.swift
+++ src/Day01.swift
@@ -1,11 +1,56 @@
+let numberMap: [String: Int] = [
+  "zero": 0,
+  "one": 1,
+  "two": 2,
+  "three": 3,
+  "four": 4,
+  "five": 5,
+  "six": 6,
+  "seven": 7,
+  "eight": 8,
+  "nine": 9,
+]
+
 public func part1(lines: [String]) -> Int {
-  var result: Int = 0
+  return findCalibration(lines: lines)
+}
+
+public func part2(lines: [String]) -> Int {
+  return findCalibration(lines: lines, findString: true)
+}
+
+func findCalibration(lines: [String], findString: Bool = false) -> Int {
+  var result = 0
+
   for line in lines {
-    if let firstChar = line.first(where: { $0.isNumber }), let lastChar = line.last(where: { $0.isNumber }) {
-        if let firstNum = Int(String(firstChar)), let lastNum = Int(String(lastChar)) {
-            result += firstNum * 10 + lastNum
+    var digits: [Int] = []
+    for index in line.indices {
+      let char = line[index]
+      if char.isNumber {
+        digits.append(Int(String(char))!)
+      } else if findString {
+        let remaining = line[index...]
+        if let num = wordToNum(string: String(remaining), dictionary: numberMap) {
+          digits.append(num)
         }
+      }
+    }
+
+    if digits.count == 1 {
+      result += digits.first! * 11
+    } else if digits.count > 1 {
+      result += digits.first! * 10 + digits.last!
     }
   }
+
   return result
+}
+
+func wordToNum(string: String, dictionary: [String: Int]) -> Int? {
+  for (word, num) in dictionary {
+    if string.hasPrefix(word) {
+      return num
+    }
+  }
+  return nil
 }
