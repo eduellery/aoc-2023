8b3b314e997106c735f29bd56f589e2626e90a68
Eduardo Ellery <eduellery@gmail.com>
2023-12-27 15:21:03 +0100

    Remove input files

---
+++ get_input.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+YR=2023
+DAY=$1
+
+if [ -z "$DAY" ]; then
+    DAY=$(date +%e | tr -d " ")
+fi
+
+if [ -r .aocsession ]; then
+    AOC_SESSION=$(cat .aocsession)
+fi
+
+if [ -z "$AOC_SESSION" ]; then
+    echo "no session found"
+    exit 1
+fi
+
+D2=$(printf "%02d" $DAY)
+
+echo "getting puzzle input for day $D2 for year $YR"
+
+(
+UA="https://github.com/eduellery/aoc-2023"
+curl -s https://adventofcode.com/$YR/day/$DAY/input --cookie "session=$AOC_SESSION" -H "User-Agent: $UA"
+) >resource/day$D2.in
