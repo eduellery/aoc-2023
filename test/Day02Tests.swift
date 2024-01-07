9f87df3dc9401bab4f71d71a6683315e7b27ab43
Eduardo Ellery <eduellery@gmail.com>
2024-01-07 06:46:12 +0100

    Make local tests conditional

--- test/Day02Tests.swift
+++ test/Day02Tests.swift
@@ -1,23 +1,27 @@
 import XCTest
+
 @testable import AoC2023
 
 final class Day02Tests: XCTestCase {
-    let test = Day02(input: getContent(path: "resource/day02.test"))
-    let input = Day02(input: getContent(path: "resource/day02.in"))
+  let local = FileManager.default.fileExists(atPath: "resource/day02.in")
+  let test = Day02(input: getContent(path: "resource/day02.test"))
+  let input = Day02(input: getContent(path: "resource/day02.in"))
 
-    func testDay02_part1_test() throws {
-        XCTAssertEqual(test.part1(), 8)
-    }
+  func testDay02_part1_test() throws {
+    XCTAssertEqual(test.part1(), 8)
+  }
 
-    func testDay02_part1_solution() throws {
-        XCTAssertEqual(input.part1(), 2528)
-    }
+  func testDay02_part1_solution() throws {
+    try XCTSkipUnless(local)
+    XCTAssertEqual(input.part1(), 2528)
+  }
 
-    func testDay02_part2_test() throws {
-        XCTAssertEqual(test.part2(), 2286)
-    }
+  func testDay02_part2_test() throws {
+    XCTAssertEqual(test.part2(), 2286)
+  }
 
-    func testDay02_part2_solution() throws {
-        XCTAssertEqual(input.part2(), 67363)
-    }
+  func testDay02_part2_solution() throws {
+    try XCTSkipUnless(local)
+    XCTAssertEqual(input.part2(), 67363)
+  }
 }
--- test/Day03Tests.swift
+++ test/Day03Tests.swift
@@ -1,23 +1,27 @@
 import XCTest
+
 @testable import AoC2023
 
 final class Day03Tests: XCTestCase {
-    let test = Day03(input: getContent(path: "resource/day03.test"))
-    let input = Day03(input: getContent(path: "resource/day03.in"))
+  let local = FileManager.default.fileExists(atPath: "resource/day03.in")
+  let test = Day03(input: getContent(path: "resource/day03.test"))
+  let input = Day03(input: getContent(path: "resource/day03.in"))
 
-    func testDay03_part1_test() throws {
-        XCTAssertEqual(test.part1(), 4361)
-    }
+  func testDay03_part1_test() throws {
+    XCTAssertEqual(test.part1(), 4361)
+  }
 
-    func testDay03_part1_solution() throws {
-        XCTAssertEqual(input.part1(), 546312)
-    }
+  func testDay03_part1_solution() throws {
+    try XCTSkipUnless(local)
+    XCTAssertEqual(input.part1(), 546312)
+  }
 
-    func testDay03_part2_test() throws {
-        XCTAssertEqual(test.part2(), 467835)
-    }
+  func testDay03_part2_test() throws {
+    XCTAssertEqual(test.part2(), 467835)
+  }
 
-    func testDay03_part2_solution() throws {
-        XCTAssertEqual(input.part2(), 87449461)
-    }
+  func testDay03_part2_solution() throws {
+    try XCTSkipUnless(local)
+    XCTAssertEqual(input.part2(), 87_449_461)
+  }
 }
